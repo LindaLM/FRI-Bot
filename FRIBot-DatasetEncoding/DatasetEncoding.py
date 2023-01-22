c79d9cfe34b3fbf3dbb1337da895cb5a8d903e1c
Juraj Oberta <jurajo80@gmail.com>
2023-01-22 22:39:34 +0100

    Updated final_dataset.

--- FRIBot-DatasetEncoding/DatasetEncoding.py
+++ FRIBot-DatasetEncoding/DatasetEncoding.py
@@ -7,7 +7,7 @@ from Vectorization.Vectorizer import Vectorizer
 # encoder.encode_feature_set('./data/komplet2.csv', './encoded/komplet2_encoded')
 
 encoder = EncoderLSTM()
-encoder.encode_feature_set('./data/komplet_dataset.csv', './encoded/komplet_dataset')
+encoder.encode_feature_set('./data/final_dataset.csv', './encoded/final_dataset')
 
 # Vectorization - Turning any sentence into one-hot encoded vector
 
